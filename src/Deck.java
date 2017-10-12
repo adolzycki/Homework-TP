22ebfdb427b54bf2612c32fcc45f80366459157a
adolzycki <a.dolzycki@gmail.com>
2017-10-12 12:15:00 +0200

    Added deck size excepion

--- src/Deck.java
+++ src/Deck.java
@@ -3,15 +3,19 @@ import java.util.Collections;
 
 public class Deck {
 
-    int size;
+    private int size;
     ArrayList<Card> deck = new ArrayList<>();
 
-    Deck(int size) {
+    Deck(int size) throws Exception {
+
+        if(size != 24 && size != 32 && size != 52)
+            throw new Exception("Wrong size");
         this.size = size;
         deckInit();
     }
 
     private void deckInit() {
+
         int i = 0;
         int j = 14;
         while(i < size) {
@@ -24,27 +28,30 @@ public class Deck {
         }
     }
 
-    public String getCard() {
+    String getCard() {
         return deck.get(deck.size()-1).toString();
     }
 
-    public void shuffle() {
+     void shuffle() {
         Collections.shuffle(deck);
     }
 
-    public void sort() {
+     void sort() {
         Collections.sort(deck);
     }
 
     public static void main(String[] args) {
 
-        Deck deck = new Deck(52);
+        try {
+            Deck deck = new Deck(52);
+            deck.shuffle();
+            System.out.println(deck.getCard());
+            deck.sort();
+            System.out.println(deck.getCard());
+        }
+        catch (Exception ignored) {
 
-        deck.shuffle();
-        System.out.println(deck.getCard());
-        deck.sort();
-        System.out.println(deck.getCard());
-
+        }
     }
 
 }
